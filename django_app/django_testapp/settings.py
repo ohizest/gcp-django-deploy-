7b746bf24b288df137318112f6bfc18ace4b4faf
Kevin Asikpata <ohiremen58@gmail.com>
2025-06-22 14:32:52 +0000

    modified ALLOWED HOSTS

--- django_app/django_testapp/settings.py
+++ django_app/django_testapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#svfxjazt^9+sj!&6nf%x-r_nykr1u)malr4xz)d06hin-)4x='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.203.79.19', 'localhost']
 
 
 # Application definition
